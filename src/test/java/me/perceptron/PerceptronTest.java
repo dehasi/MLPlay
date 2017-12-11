f70d7101681f16e720f9bceeb7126a7a8b5d1fa6
Ravil Galeyev <dehasi@yandex.ru>
2017-12-11 20:49:34 +0100

    Fix formatting

--- src/test/java/me/perceptron/PerceptronTest.java
+++ src/test/java/me/perceptron/PerceptronTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class PerceptronTest {
     @Test
     public void getW_returnsValidWeights() {
-        double w[] = new double[]{1,2,3};
+        double w[] = new double[]{1, 2, 3};
         Perceptron p = new Perceptron(w);
         Assert.assertArrayEquals(p.getW(), w, 0.01);
     }
@@ -17,7 +17,7 @@ public class PerceptronTest {
     @Test
     public void activate_returnsZero() {
         double w[] = new double[]{-5, -1, 5, 0};
-        double x[] = new double[]{1, 0, 1,1};
+        double x[] = new double[]{1, 0, 1, 1};
         Perceptron p = new Perceptron(w);
 
         Assert.assertEquals(p.activate(x), 0);
