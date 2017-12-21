9bafaddb483a9cea99a03289410a14f57206b542
Ravil Galeyev <dehasi@yandex.ru>
2017-12-21 23:00:01 +0100

    Perceptron learning refactoring

--- src/main/java/me/nn/perceptron/PerceptronLearning.java
+++ src/main/java/me/nn/perceptron/PerceptronLearning.java
@@ -8,31 +8,37 @@ import me.math.Vector;
  */
 public class PerceptronLearning {
     public double[] learn(double[][] examples, int[] responses) {
+        return learn(examples, responses, 1000);
+    }
+
+    public double[] learn(double[][] examples, int[] responses, long iterations){
         Preconditions.checkState(examples.length == responses.length, "Amount of examples and answers should be equal");
-        double[] w = new double[examples.length];
+        Preconditions.checkState(iterations > 0, "The number of iterations must be positive");
+
+        double[] weights = new double[examples.length];
         boolean perfect = false;
-        int watchDog = 1000;
+
         while (!perfect) {
-            if (watchDog < 0 ) {
+            if (iterations < 0 ) {
                 throw new IllegalStateException("It is not possible ot find solution");
             }
-            --watchDog;
+            --iterations;
 
             perfect = true;
             for (int i = 0; i < examples.length; i++) {
                 double[] e =  examples[i];
-                int predicted = predict(e,w);
+                int predicted = predict(e,weights);
                 if (predicted != responses[i]) {
                     perfect = false;
                     if (predicted == 0) {
-                        w = Vector.applyVectors(w, e, (x1, x2) -> x1 + x2);
+                        weights = Vector.applyVectors(weights, e, (x1, x2) -> x1 + x2);
                     } else {
-                        w = Vector.applyVectors(w, e, (x1, x2) -> x1 - x2);
+                        weights = Vector.applyVectors(weights, e, (x1, x2) -> x1 - x2);
                     }
                 }
             }
         }
-        return w;
+        return weights;
     }
 
     private int predict(double[] e, double[] w) {
--- src/test/groovy/me/nn/perceptron/PerceptronLearningTest.groovy
+++ src/test/groovy/me/nn/perceptron/PerceptronLearningTest.groovy
@@ -5,6 +5,10 @@ import spock.lang.Specification
 /** Test for {@link PerceptronLearning} */
 class PerceptronLearningTest extends Specification {
     PerceptronLearning learning = new PerceptronLearning()
+        double[][] examples = [[1, 1, 0.3],
+                               [1, 0.4, 0.5],
+                               [1, 0.7, 0.8]]
+        int[] responses = [1, 1, 0]
 
     def "should return zero correct weights"() {
         given:
@@ -16,16 +20,18 @@ class PerceptronLearningTest extends Specification {
     }
 
     def "should find correct weights"() {
-        given:
-        double[][] examples = [[1, 1, 0.3],
-                               [1, 0.4, 0.5],
-                               [1, 0.7, 0.8]]
-        int[] responses = [1, 1, 0]
-
         when:
         def weights = learning.learn(examples, responses)
 
         then:
         weights == [1.0, 1.6, -3.1000000000000005] as double[]
+    }
+
+    def "throw exception if it is not possible to find solution"() {
+        when:
+        learning.learn(examples, responses, 1) == [0, 0] as double[]
+
+        then:
+        IllegalStateException ex = thrown()
     }
 }
